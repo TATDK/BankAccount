252bf1452db08294173786157b55312c5f3ab79a
Thomas Andresen <thomas.andresen@earthgame.dk>
2011-03-11 23:20:46 +0100

    Spaces to tabs

--- src/dk/earthgame/TAT/BankAccount/BankAccountNPC.java
+++ src/dk/earthgame/TAT/BankAccount/BankAccountNPC.java
@@ -23,50 +23,50 @@ public class BankAccountNPC extends EntityListener {
 
 	public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
 
-        if (event.getEntity() instanceof HumanEntity) {
-            BasicHumanNpc npc = HumanNPCList.getBasicHumanNpc(event.getEntity());
+		if (event.getEntity() instanceof HumanEntity) {
+			BasicHumanNpc npc = HumanNPCList.getBasicHumanNpc(event.getEntity());
 
-            if (npc != null && event.getDamager() instanceof Player) {
+			if (npc != null && event.getDamager() instanceof Player) {
 
-                Player p = (Player) event.getDamager();
-                p.sendMessage("<" + npc.getName() + "> Don't hit me so much :P");
+				Player p = (Player) event.getDamager();
+				p.sendMessage("<" + npc.getName() + "> Don't hit me so much :P");
 
-                NpcSpawner.RemoveBasicHumanNpc(npc);
-                HumanNPCList.remove(npc.getUniqueId());
+				NpcSpawner.RemoveBasicHumanNpc(npc);
+				HumanNPCList.remove(npc.getUniqueId());
 
-                event.setCancelled(true);
+				event.setCancelled(true);
 
-            }
+			}
 
-        }
-    }
+		}
+	}
 
 	@Override
-    public void onEntityTarget(EntityTargetEvent event) {
+	public void onEntityTarget(EntityTargetEvent event) {
 
-        if (event instanceof NpcEntityTargetEvent) {
-            NpcEntityTargetEvent nevent = (NpcEntityTargetEvent)event;
+		if (event instanceof NpcEntityTargetEvent) {
+			NpcEntityTargetEvent nevent = (NpcEntityTargetEvent)event;
 
-            BasicHumanNpc npc = HumanNPCList.getBasicHumanNpc(event.getEntity());
+			BasicHumanNpc npc = HumanNPCList.getBasicHumanNpc(event.getEntity());
 
-            if (npc != null && event.getTarget() instanceof Player) {
-                if (nevent.getNpcReason() == NpcTargetReason.CLOSEST_PLAYER) {
-                    Player p = (Player) event.getTarget();
-                    p.sendMessage("<" + npc.getName() + "> Hello friend, I'm an NPC!");
-                    event.setCancelled(true);
+			if (npc != null && event.getTarget() instanceof Player) {
+				if (nevent.getNpcReason() == NpcTargetReason.CLOSEST_PLAYER) {
+					Player p = (Player) event.getTarget();
+					p.sendMessage("<" + npc.getName() + "> Hello friend, I'm an NPC!");
+					event.setCancelled(true);
 
-                } else if (nevent.getNpcReason() == NpcTargetReason.NPC_RIGHTCLICKED) {
-                    Player p = (Player) event.getTarget();
-                    p.sendMessage("<" + npc.getName() + "> You right-clicked me!");
-                    event.setCancelled(true);
+				} else if (nevent.getNpcReason() == NpcTargetReason.NPC_RIGHTCLICKED) {
+					Player p = (Player) event.getTarget();
+					p.sendMessage("<" + npc.getName() + "> You right-clicked me!");
+					event.setCancelled(true);
 
-                } else if (nevent.getNpcReason() == NpcTargetReason.NPC_BOUNCED) {
-                    Player p = (Player) event.getTarget();
-                    p.sendMessage("<" + npc.getName() + "> Stop bouncing on me!");
-                    event.setCancelled(true);
-                }
-            }
-        }
+				} else if (nevent.getNpcReason() == NpcTargetReason.NPC_BOUNCED) {
+					Player p = (Player) event.getTarget();
+					p.sendMessage("<" + npc.getName() + "> Stop bouncing on me!");
+					event.setCancelled(true);
+				}
+			}
+		}
 
-    }
+	}
 }
